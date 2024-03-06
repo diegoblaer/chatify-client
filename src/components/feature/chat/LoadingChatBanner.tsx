59e03c3aa55f176fec7ddb6c0a500d653c5dd7ea
Diego Blaer <diegoblaer@gmail.com>
2024-03-06 16:32:18 -0300

    fix

--- src/components/feature/chat/LoadingChatBanner.tsx
+++ src/components/feature/chat/LoadingChatBanner.tsx
@@ -1,10 +1,17 @@
+import Spinner from "@/components/common/Spinner";
 import Typography from "@/components/common/Typography";
 
 const LoadingChatBanner = () => {
   return (
-    <Typography variant="body" className="mb-8">
-      Connecting to the Chatify Server. his action could take up to a minute...
-    </Typography>
+    <div className="bg-white rounded p-12 text-center">
+      <Typography variant="h5" className="mb-1">
+        Connecting to the Chatify Server
+      </Typography>
+      <Typography variant="small" as={"div"} className="mb-8">
+        This action could take up to a minute...
+      </Typography>
+      <Spinner />
+    </div>
   );
 };
 
